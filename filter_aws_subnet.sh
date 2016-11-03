3309271982cd8c0dcf2e9e5216d44fb5105211c9
Bruno Bronosky <Bruno@Bronosky.com>
2016-11-03 14:31:08 -0400

    Add tool for getting values to send to subnet.py
    The commentation is very Mac specific, but I plan to rewrite this to use
    the AWS CLI anyway. This was just a "help my eyes make sense of this
    damn webpage" effort. This isn't that useful alone, but I have some
    forthcoming xargs-fu that will make it awesome.

---
+++ filter_aws_subnet.sh
@@ -0,0 +1,12 @@
+# Pipe pbpaste into this script after doing CMD+A CMD+C on...
+# https://us-west-2.console.aws.amazon.com/vpc/home?region=us-west-2#subnets:
+
+[[ $1 == '-h' || $1 == '--help' ]] && cat <<EOF && exit
+Usage:
+    pbpaste | ./filter_aws_subnet.sh
+
+    pbpaste > aws_subnets.txt
+    ./filter_aws_subnet.sh < aws_subnets.txt
+EOF
+
+awk '/\(|\|/{next}; /subnet-/{subnet=$1}; /\//{if(subnet){cidr=$1; print subnet, cidr}}'
